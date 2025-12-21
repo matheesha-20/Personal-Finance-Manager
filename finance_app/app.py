1cae216e907323df57d340293412618f5b00ab70
matheesha-20 <matheeshakavindu32@gmail.com>
2025-12-21 21:58:38 +0100

    feat: implement expense addition with date and category, enhance UI with flash messages

--- finance_app/app.py
+++ finance_app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
-import sqlite3
+from flask import Flask, render_template, request,url_for, redirect, make_response
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from flask import flash
 
 app = Flask(__name__)
 
@@ -8,8 +9,16 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finance.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'my_secret_key'
 
 db = SQLAlchemy(app)
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    date = db.Column(db.Date, nullable=False, default=db.func.current_date())
+    category = db.Column(db.String(50), nullable=False)
 
 with app.app_context():
     db.create_all()
@@ -38,6 +47,35 @@ def index():
 
     # db.close()
     return render_template("index.html")
+
+@app.route("/add", methods=["POST"])
+def add():
+    
+    name = request.form["name"]
+    amount = request.form["amount"]
+    date_str = request.form["date"]
+    category = request.form["category"]
+
+
+    date = datetime.strptime(date_str, "%Y-%m-%d").date()
+
+    new_expense = Expense(name=name, amount=amount, date=date, category=category)
+    db.session.add(new_expense)
+    db.session.commit()
+
+   
+
+    print(f"Added expense: {name}, {amount}, {date}, {category}")
+    
+    flash("Expense added successfully!", "success")
+
+    return redirect(url_for("index"))
+
+    @app.route("/")
+    def index():
+        expenses = Expense.query.order_by(Expense.date.desc()).all()
+        total = sum(e.amount for e in expenses)
+        return render_template("index.html", expenses=expenses, total=total)
 
 
 
